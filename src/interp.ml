423d07d8d3db4ab52e3f88e72270fc3cb518cb62
niris <nirisix@gmail.com>
2017-12-03 10:03:25 +0800

    backup

---
+++ src/interp.ml
@@ -0,0 +1,40 @@
+open Ast
+open Batteries
+
+type ret_value =
+    | RNil
+    | RBool of bool
+    | RStr of string
+    | RNum of float
+    | RFunc
+    | RLst of ret_value list
+
+and environment = (string * ret_value) list
+
+type ret = (environment * ret_value, string) result
+
+let rec to_string = function
+    | RNil -> ""
+    | RBool b -> P.sprintf "%B" b
+    | RStr s -> P.sprintf "%S" s
+    | RNum f -> P.sprintf "%F" f
+    | RFunc -> "#<procedure>"
+    | RLst d ->
+        let sub = String.concat " " (List.map to_string d) in
+        P.sprintf "(%s)" sub
+
+
+let env_get env k = List.assoc k env
+
+and env_set env k v = (k, v) :: env
+
+let rec eval env = function
+    | Nil -> Ok (env, RNil)
+    | Bool b -> Ok (env, RBool b)
+    | Sym k -> Ok (env, env_get env k)
+    | Str s -> Ok (env, RStr s)
+    | Num f -> Ok (env, RNum f)
+    | Lst _ -> Ok (env, RNil)
+
+
+let run exp = match eval [] exp with Ok (_, v) -> to_string v | Bad s -> s
--- src/pretty.ml
+++ src/pretty.ml
@@ -8,14 +8,17 @@ module P = Printf
 
 type doc =
     | Concat of doc list
-    | Line
-    | Indent of int
+    | Line of int
     | Text of string
 [@@deriving show]
 
 let dump (exp: doc) : string = show_doc exp
 
-let rec to_string (exp: doc) : string = ""
+let rec to_string = function
+    | Concat doc -> List.map to_string doc |> String.concat ""
+    | Line i -> "\n" ^ BatString.repeat " " i
+    | Text s -> s
+
 
 let rec from_datum = function
     | Nil -> Text ""
@@ -24,12 +27,19 @@ let rec from_datum = function
     | Str s -> Text (P.sprintf "%S" s)
     | Num f -> Text (P.sprintf "%F" f)
     | Lst d ->
-        let sub = List.map from_datum d in
+        let sub =
+            d |> List.map from_datum
+            |> List.map (function
+                | Concat d -> d
+                | Text s -> [Text s; Text " "]
+                | Line i -> [Line i] )
+            |> List.flatten |> List.map (function Line i -> Line (i + 4) | x -> x)
+        in
         let sub2 = Concat sub in
-        Concat [Text "("; Line; sub2; Line; Text ")"]
+        Concat [Text "("; Line 4; sub2; Line 0; Text ")"]
 
 
 let print (exp: datum) : string =
     let d = from_datum exp in
-    dump d
+    to_string d
 
