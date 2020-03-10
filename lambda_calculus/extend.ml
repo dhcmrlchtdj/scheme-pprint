8a1b2b691f726a55ca09294af302c665fedbb1e7
h11 <nirisix@gmail.com>
2020-03-10 23:58:32 +0800

    extend

---
+++ lambda_calculus/extend.ml
@@ -0,0 +1,43 @@
+type types =
+  [ `Bool
+  | `Int
+  | `Float
+  | `String
+  | `Arrow of types * types
+  | `Unit
+  | `UserDef of string
+  | `Tuple of types list
+  | `Record of (string * types) list
+  | `Variant of (string * types) list
+  | `List of types
+  ]
+
+type terms =
+  [ `Var of string
+  | `Abs of string * types * terms
+  | `App of terms * terms
+  | `Unit
+  | `As of string * types
+  | `Let of string * terms * terms
+  | `Tuple of terms list
+  | `TupleItem of terms * terms
+  | `Record of (string * terms) list
+  | `RecordItem of terms * terms
+  | `Tagging of string * string * types
+  | `Case of terms * (string * string * terms) list
+  | `Fix of terms
+  | `ListNil of types
+  | `ListCons of types * terms * terms
+  | `ListIsNil of types * terms
+  | `ListHead of types * terms
+  | `ListTail of types * terms
+  ]
+
+type values =
+  [ `Abs of string * types * terms
+  | `Unit
+  | `Tuple of values list
+  | `Record of (string * values) list
+  | `ListNil of types
+  | `ListCons of types * terms * terms
+  ]
